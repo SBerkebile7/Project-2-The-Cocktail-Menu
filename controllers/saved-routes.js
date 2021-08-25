0b51c5abbb3401a44b53cdfb8fd8a1149f4d665c
Spenny Berks <sberkebile@live.com>
2021-08-25 11:42:09 -0400

    fixed pathing issue for saved posts and contact page. Need to implement a 'save-post' butbutton and allow those posts to appear on saved posts page

---
+++ controllers/saved-routes.js
@@ -0,0 +1,34 @@
+const router = require('express').Router();
+const sequelize = require('../config/connection');
+const { Post, User } = require('../models');
+const withAuth = require('../utils/auth');
+
+router.get('/', withAuth, (req,res) => {
+    Post.findAll({
+        where: {
+            user_id: req.session.user_id
+        },
+        attributes: [
+            'id',
+            'title',
+            'created_at',
+            'post_content'
+        ],
+        include: [
+            {
+                model: User,
+                attributes: ['username']
+            }
+        ]
+    })
+    .then(dbPostData => {
+        const posts = dbPostData.map(post => post.get({ plain: true }));
+        res.render('saved', { posts, loggedIn: true });
+    })
+    .catch(err => {
+        console.log(err);
+        res.status(500).json(err);
+    });
+});
+
+module.exports = router;
