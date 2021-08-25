dd7efc7493b89ee50e49e28f08ff615d048b208d
ryanjolley12 <ryanswims@gmail.com>
2021-08-25 19:33:02 -0400

    debugged

--- server.js
+++ server.js
@@ -2,8 +2,7 @@ const path = require('path');
 const express = require('express');
 const session = require('express-session');
 const exphbs = require('express-handlebars');
-// added bootstrap
-const bootstrap = require('bootstrap');
+
 
 const app = express();
 const PORT = process.env.PORT || 3001;
