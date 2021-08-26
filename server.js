5bc233fedfd73c4ca425468827b374e06a3591b3
ryanjolley12 <ryanswims@gmail.com>
2021-08-25 23:09:47 -0400

    moved images photo

--- server.js
+++ server.js
@@ -34,6 +34,7 @@ app.use(express.urlencoded({ extended: true }));
 app.use(express.static(path.join(__dirname, 'public')));
 
 app.use(require('./controllers/'));
+app.use(express.static('views/images')); 
 
 sequelize.sync({ force: false }).then(() => {
   app.listen(PORT, () => console.log('Now listening'));
